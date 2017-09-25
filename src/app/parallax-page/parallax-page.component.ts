4accdbc3838d0059397e55cec46f8ab450e63499
laventy <xiaoleil3@student.unimelb.edu.au>
2017-09-25 18:33:22 +1000

    add some scroll reveal effect

--- src/app/parallax-page/parallax-page.component.ts
+++ src/app/parallax-page/parallax-page.component.ts
@@ -7,27 +7,57 @@ import { Component,
          keyframes,
          OnInit } from '@angular/core';
 
+// Declare jquery
+declare var $:any;
+
+// Declare PhotoSwipe for Moment section
+declare var initPhotoSwipeFromDOM: any;
+
+// Declare masonry-related
+declare var imagesLoaded: any;
+declare var masonry: any;
+
 @Component({
   selector: 'app-parallax-page',
   templateUrl: './parallax-page.component.html',
   styleUrls: ['./parallax-page.component.css'],
-  animations: [
-    trigger('movePanel', [
-            transition('void => *', [
-                animate(1000, keyframes([
-                    style({opacity: 0, transform: 'translateY(-40px)', offset: 0}),
-                    style({opacity: 1, transform: 'translateY(0)', offset: 1}),
-                ]))
-            ])
+  // animations: [
+  //   trigger('moveDown', [
+  //           transition('void => *', [
+  //               animate(1000, keyframes([
+  //                   style({opacity: 0, transform: 'translateY(-40px)', offset: 0}),
+  //                   style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+  //               ]))
+  //           ])
 
-        ])
-  ]
+  //   ]),
+
+  //   trigger('moveRight', [
+  //     transition('void => *', [
+  //         animate(1500, keyframes([
+  //             style({opacity: 0, transform: 'translateX(-40px)', offset: 0}),
+  //             style({opacity: 1, transform: 'translateX(0)', offset: 1}),
+  //         ]))
+  //     ])
+
+  //   ]),
+  // ]
 })
 export class ParallaxPageComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  }
+    // Initial PhotoSwipe library
+    initPhotoSwipeFromDOM('.my-gallery');
 
+    // init Masonry after all images have loaded
+    var $grid = $('.grid').imagesLoaded( function() {
+      $grid.masonry({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        columnWidth: '.grid-sizer',
+      }); 
+    });
+  }
 }
